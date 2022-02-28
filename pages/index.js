5c21deac7a27753df078f840d8e312840d858131
Jim Hartford <jim.hartford@tradeglobal.com>
2022-02-28 08:25:42 -0500

    Added facebook and instagram

--- pages/index.js
+++ pages/index.js
@@ -61,16 +61,16 @@ export default function ProfilePage({ ...rest }) {
                 <i className="fab fa-twitter" /> Connect on twitter
               </Button>
             </div>
-            {/* <div>
-              <Button color="facebook" style={{minWidth: '300px'}}>
+            <div>
+              <Button color="facebook" style={{minWidth: '300px'}} onClick={() => {window.open('https://www.facebook.com/malik.hartford.9','_blank')}}>
                 <i className="fab fa-facebook-square" /> Connect on Facebook
               </Button>
             </div>
             <div>
-              <Button color="instagram" style={{minWidth: '300px'}}>
+              <Button color="instagram" style={{minWidth: '300px'}} onClick={() => {window.open('https://www.instagram.com/malik.hartford/','_blank')}}>
                 <i className="fab fa-instagram" /> Connect on Instgram
               </Button>
-            </div> */}
+            </div>
             <div>
               <Button color="youtube" style={{minWidth: '300px'}} onClick={() => {window.open('https://www.youtube.com/channel/UCOeA4NU8uPT6QwCFXspvATQ','_blank')}}>
                 <i className="fab fa-youtube" /> View On Youtube
