41486d493550519a6a32da4e3af82f30cc6ed92d
Jim Hartford <jim.hartford@tradeglobal.com>
2022-03-02 18:14:02 -0500

    added additional datalayer info

--- pages/index.js
+++ pages/index.js
@@ -52,31 +52,31 @@ export default function ProfilePage({ ...rest }) {
           </GridContainer>
           <br />
           <GridContainer justify="center">
-              <Button className="hudl" id="hudl" color="hudl" style={{minWidth: '300px', maxWidth: '300px'}} onClick={() => {dataLayer.push({'event': 'hudl_click'});window.open('https://www.hudl.com/profile/12603031/Malik-Hartford','_blank')}}>
+              <Button onClick={() => {dataLayer.push({'event': 'outbound_click', 'location': 'hudl'});window.open('https://www.hudl.com/profile/12603031/Malik-Hartford','_blank')}} className="hudl" id="hudl" color="hudl" style={{minWidth: '300px', maxWidth: '300px'}}>
                 <img src="/img/malik/hudl-icon.svg" type="image/svg+xml" style={{minHeight: '17px', maxHeight: '17px'}}></img>&nbsp;&nbsp;View On Hudl
               </Button>
-              <Button className="buttonLink" id="twitter" color="twitter" style={{minWidth: '300px'}} onClick={() => {dataLayer.push({'event': 'twitter_click'});window.open('https://twitter.com/MalikHartford','_blank')}}>
+              <Button onClick={() => {dataLayer.push({'event': 'outbound_click', 'location': 'twitter'});window.open('https://twitter.com/MalikHartford','_blank')}} className="buttonLink" id="twitter" color="twitter" style={{minWidth: '300px'}}>
                 <i className="fab fa-twitter" /> Connect on twitter
               </Button>
-              <Button className="buttonLink" id="instagram" color="instagram" style={{minWidth: '300px'}} onClick={() => {dataLayer.push({'event': 'instagram_click'});window.open('https://www.instagram.com/malik.hartford/','_blank')}}>
+              <Button onClick={() => {dataLayer.push({'event': 'outbound_click', 'location': 'instagram'});window.open('https://www.instagram.com/malik.hartford/','_blank')}} className="buttonLink" id="instagram" color="instagram" style={{minWidth: '300px'}}>
                 <i className="fab fa-instagram" /> Connect on Instagram
               </Button>
-              <Button className="buttonLink" id="247sports" color="behance" style={{minWidth: '300px'}} onClick={() => {dataLayer.push({'event': '247sports_click'});window.open('https://247sports.com/Player/Malik-Hartford-46125498/','_blank')}}>
+              <Button onClick={() => {dataLayer.push({'event': 'outbound_click', 'location': '247sports'});window.open('https://247sports.com/Player/Malik-Hartford-46125498/','_blank')}} className="buttonLink" id="247sports" color="behance" style={{minWidth: '300px'}}>
                 <img src="/img/malik/247sports.svg" type="image/svg+xml" style={{minHeight: '17px', maxHeight: '17px'}}></img>&nbsp;&nbsp;View on 247sports
               </Button>
-              <Button className="buttonLink" id="rivals" color="facebook" style={{minWidth: '300px'}} onClick={() => {dataLayer.push({'event': 'rivals_click'});window.open('https://n.rivals.com/content/prospects/2023/malik-hartford-280084','_blank')}}>
+              <Button onClick={() => {dataLayer.push({'event': 'outbound_click', 'location': 'rivals'});window.open('https://n.rivals.com/content/prospects/2023/malik-hartford-280084','_blank')}} className="buttonLink" id="rivals" color="facebook" style={{minWidth: '300px'}}>
                 <img src="/img/malik/rivals.svg" type="image/svg+xml" style={{minHeight: '14px', maxHeight: '14px'}}></img>&nbsp;&nbsp;View on rivals
               </Button>
-              <Button className="buttonLink" id="on3" color="github" style={{minWidth: '300px'}} onClick={() => {dataLayer.push({'event': 'on3_click'});window.open('https://www.on3.com/db/malik-hartford-147153/','_blank')}}>
+              <Button onClick={() => {dataLayer.push({'event': 'outbound_click', 'location': 'on3'});window.open('https://www.on3.com/db/malik-hartford-147153/','_blank')}} className="buttonLink" id="on3" color="github" style={{minWidth: '300px'}}>
                 <img src="/img/malik/on3.svg" type="image/svg+xml" style={{minHeight: '17px', maxHeight: '17px', color: '#FFFFFF'}}></img>&nbsp;&nbsp;View on on3
               </Button>
-              <Button className="buttonLink" id="espn" color="pinterest" style={{minWidth: '300px'}} onClick={() => {dataLayer.push({'event': 'espn_click'});window.open('http://www.espn.com/college-sports/football/recruiting/player/_/id/249823/malik-hartford','_blank')}}>
+              <Button onClick={() => {dataLayer.push({'event': 'outbound_click', 'location': 'espn'});window.open('http://www.espn.com/college-sports/football/recruiting/player/_/id/249823/malik-hartford','_blank')}} className="buttonLink" id="espn" color="pinterest" style={{minWidth: '300px'}}>
                 <img src="/img/malik/espn-logo.svg" type="image/svg+xml" style={{minHeight: '17px', maxHeight: '17px'}}></img>&nbsp;&nbsp;View on ESPN
               </Button>
-              <Button className="buttonLink" id="facebook" color="facebook" style={{minWidth: '300px'}} onClick={() => {dataLayer.push({'event': 'facebook_click'});window.open('https://www.facebook.com/malik.hartford.9','_blank')}}>
+              <Button onClick={() => {dataLayer.push({'event': 'outbound_click', 'location': 'facebook'});window.open('https://www.facebook.com/malik.hartford.9','_blank')}} className="buttonLink" id="facebook" color="facebook" style={{minWidth: '300px'}}>
                 <i className="fab fa-facebook-square" /> Connect on Facebook
               </Button>
-              <Button className="buttonLink" id="youtube" color="youtube" style={{minWidth: '300px'}} onClick={() => {dataLayer.push({'event': 'youtube_click'});window.open('https://www.youtube.com/user/KingJamesMMV','_blank')}}>
+              <Button onClick={() => {dataLayer.push({'event': 'outbound_click', 'location': 'youtube'});window.open('https://www.youtube.com/user/KingJamesMMV','_blank')}} className="buttonLink" id="youtube" color="youtube" style={{minWidth: '300px'}}>
                 <i className="fab fa-youtube" /> View On Youtube
               </Button>
           </GridContainer>
